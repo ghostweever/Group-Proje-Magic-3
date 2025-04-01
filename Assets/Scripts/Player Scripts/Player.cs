8605a1eae97e8c8e59fbb8032924d2474ac0eae6
BranBuddy <brandonwahl111@gmail.com>
2025-03-31 21:24:57 -0400

    Revert "added spell manager"
    This reverts commit 49acdaa9f47ae39fa596f8fe5f3009dc0bda5d96.

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -5,17 +5,52 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private bool isGrounded;
-    private SpellManager spellManager;
 
     void Start()
     {
-        spellManager = GetComponent<SpellManager>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        spellManager.WhichSpellToCast();
-    }    
+        if (Input.GetKeyDown(KeyCode.Q) && isGrounded == false)
+        {
+            GameObject.Find("Player").GetComponent<MakeCloud>().CloudSpell();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject.Find("Player").GetComponent<MakeWater>().WaterSpell();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject.Find("Player").GetComponent<MakeVineWhip>().VineSpell();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            isGrounded = true;
+
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            isGrounded = false;
+
+
+        }
+    }
+    
 
 }
