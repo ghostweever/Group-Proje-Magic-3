727087252d7d0014e53196fea8d223ce749dcce8
Gregory Johnson <gr845667@ucf.edu>
2025-04-02 16:28:01 -0400

    Dash
    Need to adjust numbers, but it works

--- Assets/Scripts/Spell Scripts/Dash.cs
+++ Assets/Scripts/Spell Scripts/Dash.cs
@@ -7,31 +7,47 @@ public class Dash : MonoBehaviour
     
 {
 
-    //
-    //
-    //
-    //
-  
+    public float dashSpeed = 10f;
+    public float dashLength = 5f;
+    public float dashCooldown = 1f;
+
+    private float dashTimer;
+    private bool isDashing;
+    private bool canDash = true;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.B) && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            dashTimer = 0f;
+        }
 
+        if (isDashing)
+        {
+            // Move the character during the dash
+            CharacterController characterController = GetComponent<CharacterController>();
+            if (characterController != null)
+            {
+                characterController.Move(transform.forward * dashSpeed * Time.deltaTime);
+            }
+            dashTimer += Time.deltaTime;
+            if (dashTimer >= dashLength)
+            {
+                isDashing = false;
+                dashTimer = 0f;
+                Invoke("ResetDash", dashCooldown);
+            }
+        }
+    }
 
-
-
-
-    
-            
-            
-        
+    void ResetDash()
+    {
+        canDash = true;
     }
 
 
