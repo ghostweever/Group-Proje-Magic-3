954a7f06a55284216a02f0129a574158a36981e0
ghostweever <149333742+ghostweever@users.noreply.github.com>
2025-04-05 17:07:44 -0400

    i dunno, downloaded everythign

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+using Color = System.Drawing.Color;
+>>>>>>> Stashed changes
 
 public class PlayerMovement : MonoBehaviour
 {
