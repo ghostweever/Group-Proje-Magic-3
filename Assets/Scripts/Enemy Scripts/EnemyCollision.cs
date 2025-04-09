d4001e44266ccd45b400fae6daac1232700aefa3
BranBuddy <brandonwahl111@gmail.com>
2025-04-08 21:10:21 -0400

    fixes

--- Assets/Scripts/Enemy Scripts/EnemyCollision.cs
+++ Assets/Scripts/Enemy Scripts/EnemyCollision.cs
@@ -6,10 +6,13 @@ public class EnemyCollision : MonoBehaviour
 {
     
     private Crystal crystal;
+    public int whatEnemyAmI;
+    public int enemyLives;
 
     void Start()
     {
         crystal = GetComponent<Crystal>();
+        WhoAmI();
     }
 
     // Update is called once per frame
@@ -17,23 +20,49 @@ public class EnemyCollision : MonoBehaviour
     {
         
     }
-
-    private void OnCollisionEnter(Collision collision)
+    void WhoAmI()
     {
-        if (collision.collider.tag == "Weapon")
+        if (whatEnemyAmI == 0)
         {
+            enemyLives = 1;
+        }
+        else if (whatEnemyAmI == 1)
+        {
+            enemyLives = 5;
+
+        }
+    }
+
+    void EnemyLives(int amount)
+    {
+        enemyLives -= amount;
+
+        if (enemyLives <= 0)
+        {
+            Destroy(gameObject);
             GameObject.Find("Player").GetComponent<PlayerMana>().EarnMana(25);
             GameObject.Find("GameManager").GetComponent<GameManager>().Score(100);
             GameObject.Find("CrystalHolder").GetComponent<Crystal>().ActivateCrystal();
-            Destroy(gameObject);
-
-        }
-
-        if(collision.collider.tag == "Player")
-        {
-            GameObject.Find("Void").GetComponent<MenuButtons>().GameOver();
         }
     }
 
 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Weapon")
+        {
+
+            EnemyLives(1);
+            Debug.Log(enemyLives);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            //GameObject.Find("Void").GetComponent<MenuButtons>().GameOver();
+        }
+    }
 }
+        
