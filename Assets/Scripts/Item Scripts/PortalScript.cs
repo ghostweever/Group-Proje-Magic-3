967300e8a4cb886babaaaf7f15e551495c04fcc5
BranBuddy <brandonwahl111@gmail.com>
2025-04-10 00:25:35 -0400

    fixed portals and added boss type

--- Assets/Scripts/Item Scripts/PortalScript.cs
+++ Assets/Scripts/Item Scripts/PortalScript.cs
@@ -1,44 +1,37 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-//public class PortalScript : MonoBehaviour
-//{
-//    private PlayerCrystalManager playerCrystalManager;
-//    public GameObject portal;
-    
+public class PortalScript : MonoBehaviour
+{
+    private PlayerCrystalManager playerCrystalManager;
+    public GameObject portal;
 
-//    void Start()
-//    {
-//        playerCrystalManager = GetComponent<PlayerCrystalManager>();
-//    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        ActivatePortal();
-        
-//    }
+    void Start()
+    {
+        playerCrystalManager = GetComponent<PlayerCrystalManager>();
+    }
 
-//    void ActivatePortal()
-//    {
-//        if (GameObject.Find("Player").GetComponent<PlayerCrystalManager>().WinCondition()){ 
-        
-//            portal.gameObject.SetActive(true);
-//        } 
-//        else
-//        {
-//            portal.gameObject.SetActive(false);
-//        }
-//    }
+    // Update is called once per frame
+    void Update()
+    {
+        ActivatePortal();
 
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if(other.tag == "Player" && GameObject.Find("Player").GetComponent<PlayerCrystalManager>().WinCondition())
-//        {
-//            SceneManager.LoadScene("Win");
-//        }
-//    }
+    }
 
-//}
+    void ActivatePortal()
+    {
+        if (GameObject.Find("Player").GetComponent<PlayerCrystalManager>().WinCondition())
+        {
+
+            portal.gameObject.SetActive(true);
+        }
+        else
+        {
+            portal.gameObject.SetActive(false);
+        }
+    }
+
+}
