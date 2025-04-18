b04bf98abdb9408bd2a7a7ffc7716bbb820e080f
BranBuddy <brandonwahl111@gmail.com>
2025-04-17 22:40:20 -0400

    added inv frames

--- Assets/Scripts/Player Scripts/PlayerLives.cs
+++ Assets/Scripts/Player Scripts/PlayerLives.cs
@@ -6,10 +6,16 @@ public class PlayerLives : MonoBehaviour
 {
 
     public int playerLives;
+    public bool isInvincible;
 
     void Start()
     {
         playerLives = 3;        
+    }
+
+    void Update()
+    {
+        FlashFrames();
     }
 
     public void Damage(int amount)
@@ -23,4 +29,36 @@ public class PlayerLives : MonoBehaviour
 
     }
 
+    public void FlashFrames()
+    {
+        if (isInvincible)
+        {
+            StartCoroutine(Flash());
+        } else
+        {
+            transform.GetChild(3).gameObject.SetActive(true);
+        }
+    }
+
+   public IEnumerator Invincible()
+    {
+        isInvincible = true;
+        Debug.Log(isInvincible);
+        yield return new WaitForSeconds(3);
+        isInvincible = false;
+        Debug.Log(isInvincible);
+    }
+
+
+
+    private IEnumerator Flash()
+    {
+        for (int i = 0; i < 20; i++) {
+            i++;
+        transform.GetChild(3).gameObject.SetActive(false);
+            yield return new WaitForSeconds(.1f);
+            transform.GetChild(3).gameObject.SetActive(true);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 }
