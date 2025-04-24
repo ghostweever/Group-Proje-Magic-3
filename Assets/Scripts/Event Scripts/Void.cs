635cef8d8c89d9a631619ed6286afe765f2cba14
BranBuddy <brandonwahl111@gmail.com>
2025-04-24 12:50:40 -0400

    voids

--- Assets/Scripts/Event Scripts/Void.cs
+++ Assets/Scripts/Event Scripts/Void.cs
@@ -7,19 +7,15 @@ public class Void : MonoBehaviour
 
     public GameObject player;
     public GameObject spawnPos;
-    public GameObject transition;
 
-    private void Start()
-    {
-     transition.GetComponent<Animator>();
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-
             player.transform.position = spawnPos.transform.position;
+            
         }
     }
+
 }
